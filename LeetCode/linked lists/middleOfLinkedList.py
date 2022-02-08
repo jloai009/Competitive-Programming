2b4afa4bd3e3c90fcd265982463c2a4935779c8c
Jose <jloai009@fiu.edu>
2022-02-08 17:11:58 -0500

    Finds middle of a linked list

---
+++ LeetCode/linked lists/middleOfLinkedList.py
@@ -0,0 +1,8 @@
+class Solution:
+    def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        slow = fast = head
+        
+        while fast and fast.next:
+            slow, fast = slow.next, fast.next.next
+            
+        return slow
